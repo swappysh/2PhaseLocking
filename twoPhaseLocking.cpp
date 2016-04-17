b0eafc44f6173b0ea1ccbb8bf5a2c44f6e11613f
swapsharma <mradul.master@gmail.com>
2016-04-17 16:03:04 +0530

    Corrected std input

--- twoPhaseLocking.cpp
+++ twoPhaseLocking.cpp
@@ -1,16 +1,21 @@
 /*
 * @Author: doody
 * @Date:   2016-04-17 14:04:33
-* @Last Modified by:   doody
-* @Last Modified time: 2016-04-17 14:15:25
+* @Last Modified by:   swapsharma
+* @Last Modified time: 2016-04-17 16:02:23
 * @Aim: To design and implement Two phase locking algo
 * @Bugs:
 * 	Only Write and Read as permissible value
 * 	Abort is the only deadlock resolution method
+* 	Nonrecommended random function added
+* 	Input method is not perfect
 */
 
 #include <iostream>
 #include <vector>
+
+#include <ctime>
+#include <cstdlib>
 
 // Definition for Read op and write op
 #define W 1
@@ -32,10 +37,12 @@ int main()
 
 	// Loop for input from user
 	char ch;
-	cin >> ch;
 	while(!cin.eof())
 	{
 		vector<instr> transaction;
+
+		// Taking the first input, ignoring whitespace
+		do {ch = cin.get();} while(ch == ' ');
 		while(!cin.eof() and ch != '\n')
 		{
 			instr tempInstr;
@@ -45,18 +52,32 @@ int main()
 			tempInstr.op = ch == 'W'?1:0;
 			
 			// Taking input for variable
-			cin >> ch;
+			do {ch = cin.get();} while(ch == ' ');
 			tempInstr.var = ch;
 		
 			// Pushing the new intruction
 			transaction.push_back(tempInstr);
 
 			// Taking input for operation
-			cin >> ch;
+			do {ch = cin.get();} while(ch == ' ');
 		}
 		// Pushing the new transaction in transaction list
 		transList.push_back(transaction);
 	}
+
+	// Randomly select the instruction to be executed
+	// keeping in mind that instructions from a transaction
+	// run in the order present in the transaction
+
+	// Seeding the random number gererator
+	srand(time(0));
+
+	// Randomly select the transaction to next go on to the
+	// butcher list. It is to be noted that rand() function
+	// provides with random number fro 0 to RAND_MAX
+	int randTransIndex = transList.size()*rand()/RAND_MAX;
+
+	cout << (transList.size()*rand())/RAND_MAX << '\n';
 
 	return 0;
 }
